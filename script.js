6603fc9d1f6e809da79422aab03cfc8ac0ed72f4
unluckyhappyy <unluckyhappy@gmail.com>
2021-07-07 00:20:12 +0300

    доделки

---
+++ script.js
@@ -0,0 +1,33 @@
+const link = document.querySelector(".header-login-link");
+const popup = document.querySelector(".modal-link");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.toggle("modal-show");
+});
+
+
+const search = document.querySelector(".search");
+const searchPopup = document.querySelector(".search-modal");
+
+search.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  searchPopup.classList.toggle("search-modal-show");
+});
+
+
+const feedback = document.querySelector(".contacts-button");
+const feedbackPopup = document.querySelector(".feedback-modal");
+const closeModal = document.querySelector(".close-modal");
+
+feedback.addEventListener("click", function(evt) {
+evt.preventDefault();
+feedbackPopup.classList.add("feedback-modal-show");
+});
+
+closeModal.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  feedbackPopup.classList.remove("feedback-modal-show");
+  });
+
+
